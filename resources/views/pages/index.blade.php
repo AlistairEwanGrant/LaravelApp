62f22efe2f6140175560420bdaeb25d66f0e1eb3
Alistair <alistairgrant95@gmail.com>
2022-03-18 17:45:45 +0000

    bg colour change

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,8 @@
 @extends("layouts.app")
 
 @section('content')
-    <div class="mt-4 p-5 bg-info bg-opacity-50 text-black rounded text-center border border-success border-5 fw-bolde ">
-        <h1 class="text-decoration-underline">Welcome To LSAPP Blog!</h1>
+    <div class="mt-4 p-5 bg-success bg-opacity-50 text-black rounded text-center border border-success border-5 fw-bolde ">
+        <h1 class="text-white">Welcome To LSAPP Blog!</h1>
         <h3>This is a blog created using Laravel 8.</h3>
         <h3>Please login or register if you do not have an account.</h3>
         <h3>You can read the blog as a guest but not submit.</h3>
