b559fa99a3c619844acd18398c2655e94e12ea9a
Alistair <alistairgrant95@gmail.com>
2022-03-18 19:23:35 +0000

    Working but not edit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,21 @@
 @extends("layouts.app")
 
 @section('content')
-    <h1 style="margin-top:5px" class="bg-success bg-opacity-50 text-white rounded text-center border border-dark fw-bolde fs-3 fw-bold">Posts</h1>
+    <h1 style="margin-top:5px" class="bg-success bg-opacity-50  rounded text-center border border-dark fw-bolde fs-3 fw-bold">Posts</h1>
     @if(count($posts) > 0)
         @foreach($posts as $post)
             <div  class="card-header width80 border border-dark rounded-3 bg-success bg-opacity-25">
                 <div class="row">
                     <div class="col-md-4 col-sm4">
-                        <a class="text-white text-decoration-none" href="posts/{{$post->id}}">
+                        <a class=" text-decoration-none" href="posts/{{$post->id}}">
                         <img style="width:100%; max-height:400px" class="border border-secondary" src="storage/cover_images/{{$post->cover_image}}">
                         </a>
                     </div>
                     <div class="col-md-8 col-sm8">
-                        <h3><a class="text-white text-decoration-none" href="posts/{{$post->id}}">{{$post->title}}</a></h3>
+                        <h3><a class=" text-decoration-none text-success" href="posts/{{$post->id}}">{{$post->title}}</a></h3>
                     {{$post->body}}
                     <br>
-                        <small class="text-white">Writen on {{$post -> created_at}} by {{$post->user->name}}</small>
+                        <small class="">Writen on {{$post -> created_at}} by {{$post->user->name}}</small>
                     </div>
                 </div>
             </div>    
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,35 +2,51 @@
 
 @section('content')
     <a style="margin-top:5px" href="/lsapp/public/posts" class="btn btn-danger">Back</a>
-    <h1 style="margin-top:5px" class="bg-info bg-opacity-25 text-white rounded text-center border border-3 border-dark fw-bolde fs-3 fw-bold">{{$post->title}}</h1>
-    <img style="width:100%" class="border border-dark rounded-3 bg-success" src="../storage/cover_images/{{$post->cover_image}}">
+    <h1 style="margin-top:5px" class="bg-success bg-opacity-50 rounded text-center border border-3 border-dark fw-bolde fs-3 fw-bold">{{$post->title}}</h1>
+        @if($post->cover_image == 'noimage.jpg')
+          
+        @else
+        <div style="text-align: center">
+            <img style="width:auto%; max-height:500px" class="border border-dark rounded-3 bg-success" src="../storage/cover_images/{{$post->cover_image}}">
+        </div>
+        @endif
     <br>
     <br>
     <div>
         <h3>{!!$post->body!!}</h3>
     </div>
-    <div class="bg-primary border border-5 border-dark rounded-3 bg-opacity-25">
-        <img style="max-width: 300px; padding:5px" src="../storage/cover_images/{{$post->user->cover_image}}">
-    
-        <small class="text-white">Written on {{$post->created_at}} by {{$post->user->name}} / email: {{$post->user->email}}</small>
-    <br>
-        <div style="margin-top:5px; padding:5px" class="text-white">
-            <small class="text-white text-wrap ">{{$post->user->bio_text}}</small>
+    <div class="bg-success bg-opacity-50 border border-5 border-dark rounded-3 bg-opacity-25 row">
+        <div class="col-md-4 col-sm4">
+            
+            <img style="max-width: 300px; padding:5px" src="../storage/cover_images/{{$post->user->cover_image}}">
+                
+            
+        </div>
+        <div class="col-md-8 col-sm8">
+            <h3 style="padding:5px" class="text-wrap">{{$post->user->bio_text}}</h3>
         </div>
     </div>
+    <br>
+        <div style="margin-top:5px; padding:5px">
+            <h5>Written on {{$post->created_at}} by {{$post->user->name}} / email: {{$post->user->email}}</h5>
+        </div>
+    
+    
     <hr>
 
 
     @if (!Auth::guest())
         @if(Auth::user()->id == $post->user_id)
+        
         <a href="/lsapp/public/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 
         {!! Form::open(['PostsController' => 'destroy', $post->id, 'class' => 'float-end'])!!}
             @method('DELETE')
             {!! Form::submit('Delete', ['class' => 'btn btn-danger'])!!}
         {!!Form::close()!!}
+        </div>
         @endif
     @endif
     <hr>
-    
+</div>
 @endsection
