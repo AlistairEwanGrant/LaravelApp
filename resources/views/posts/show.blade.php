bceb94c77cc2ad71eb9191b1815be23ba4ea0d46
Alistair <alistairgrant95@gmail.com>
2022-03-18 17:06:53 +0000

    saved before adding new table

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,16 +1,18 @@
 @extends("layouts.app")
 
 @section('content')
-    <a href="/lsapp/public/posts" class="btn btn-success">Back</a>
-    <h1>{{$post->title}}</h1>
-    <img style="width:100%" src="../storage/cover_images/{{$post->cover_image}}">
+    <a style="margin-top:5px" href="/lsapp/public/posts" class="btn btn-danger">Back</a>
+    <h1 style="margin-top:5px" class="bg-success bg-opacity-50 text-white rounded text-center border border-dark fw-bolde fs-3 fw-bold">{{$post->title}}</h1>
+    <img style="width:100%" class="border border-dark rounded-3 bg-success" src="../storage/cover_images/{{$post->cover_image}}">
     <br>
     <br>
     <div>
-        {!!$post->body!!}
+        <h3>{!!$post->body!!}</h3>
     </div>
     <hr>
-    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+    <small class="text-white">Written on {{$post->created_at}} by {{$post->user->name}}</small>
+    <hr>
+    
     <hr>
 
     @if (!Auth::guest())
