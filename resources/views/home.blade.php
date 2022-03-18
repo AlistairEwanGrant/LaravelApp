74c9348068b0740bd761835ac2888159ffd19a8a
Alistair <alistairgrant95@gmail.com>
2022-03-18 16:17:10 +0000

    pages colours edited

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="mt-4 p-5 bg-light bg-opacity-50 text-black rounded text-center border border-success border-5 fw-bolde">
+<div class="mt-4 p-5 bg-light bg-opacity-25 text-black rounded text-center border border-dark border-5 fw-bolde">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="mt-4 p-5 bg-success bg-opacity-50 text-black rounded text-center border border-success border-5 fw-bolde">
-                <div class="bg-success text-white rounded text-center border border-success fw-bolde fs-3">{{ __('Dashboard') }}</div>
+            <div class="mt-4 p-5 bg-success bg-opacity-50 text-black rounded text-center border border-dark border-5 fw-bolde">
+                <div class="bg-success text-white rounded text-center border border-dark fw-bolde fs-3">{{ __('Dashboard') }}</div>
 
                 
                 
                 {{-- img and bio --}}
                 <div class="card-header width80">
-                    <div class="row"><a href="/lsapp/public/home/{{Auth::user()->id}}/editBio" style="margin-bottom:5px"class="btn bg-primary bg-opacity-50 btn-outline-success text-white float-end ">Edit Bio</a>
+                    <div class="row"><a href="/lsapp/public/home/{{Auth::user()->id}}/editBio" style="margin-bottom:5px"class="btn bg-primary bg-opacity-50 btn-outline-dark text-white float-end ">Edit Bio</a>
                         
                         <div class="col-md-4 col-sm4">
                             <img style="width:100%" src="storage/cover_images/{{$users->cover_image}}">
                             <h5>Name</h5>
-                            <div class="bg-light bg-opacity-50 text-black rounded text-center border border-success fw-bolde">{{$users->name}}</div>
+                            <div class="bg-light bg-opacity-50 text-black rounded text-center border border-dark fw-bolde">{{$users->name}}</div>
                             <br>
                             <h5>Email</h5>
-                            <div class="bg-light bg-opacity-50 text-black rounded text-center border border-success fw-bolde">{{$users->email}}</div>
+                            <div class="bg-light bg-opacity-50 text-black rounded text-center border border-dark fw-bolde">{{$users->email}}</div>
                         </div>
                         <div class="col-md-8 col-sm8">
                             <h5>Bio</h5>
-                            <div style="height: 80%" class="bg-light bg-opacity-50 text-black rounded text-center border border-success fw-bolde">{{$users->bio_text}}</div>
+                            <div style="height: 80%" class="bg-light bg-opacity-50 text-black rounded text-center border border-dark fw-bolde">{{$users->bio_text}}</div>
                             
                                </div>
                         
@@ -38,15 +38,15 @@
                     <table class="table table-striped">
                         <tr bg-light bg-opacity-50>
                             <td></td>
-                            <td><a style="width: 100%" class="btn btn bg-primary bg-opacity-50 btn-outline-primary text-white" href="/lsapp/public/posts/create">Create Post</a></td>
+                            <td><a style="width: 100%" class="btn btn bg-primary bg-opacity-50 btn-outline-dark text-white" href="/lsapp/public/posts/create">Create Post</a></td>
                             <td></td>
                         </tr>
                         @foreach($posts as $post)
                         <tr>
-                            <td class="bg-light bg-opacity-50 text-black rounded text-center border border-success fw-bolde fs-3">{{$post->title}}</td>
-                            <td class="bg-light bg-opacity-50 text-black rounded text-center border border-success fw-bolde">{{$post->body}}</td>
+                            <td class="bg-light bg-opacity-50 text-black rounded text-center border border-dark fw-bolde fs-3">{{$post->title}}</td>
+                            <td class="bg-light bg-opacity-50 text-black rounded text-center border border-dark fw-bolde">{{$post->body}}</td>
                             <td>
-                                <a href="/lsapp/public/posts/{{$post->id}}/edit" class="btn bg-primary bg-opacity-50 btn-outline-primary text-white float-end">Edit</a>
+                                <a href="/lsapp/public/posts/{{$post->id}}/edit" class="btn bg-primary bg-opacity-50 btn-outline-dark text-white float-end">Edit</a>
                             </td>
                         </tr>
                         @endforeach
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,16 +1,16 @@
 @extends("layouts.app")
 
 @section('content')
-    <h1>Posts</h1>
+    <h1 style="margin-top:5px" class="bg-light bg-opacity-25 text-white rounded text-center border border-dark fw-bolde fs-3">Posts</h1>
     @if(count($posts) > 0)
         @foreach($posts as $post)
-            <div class="card-header width80">
+            <div  class="card-header width80 border border-dark rounded-3 bg-success bg-opacity-25">
                 <div class="row">
                     <div class="col-md-4 col-sm4">
-                        <img style="width:100%" src="storage/cover_images/{{$post->cover_image}}">
+                        <img style="width:100%; max-height:400px" class="border border-secondary" src="storage/cover_images/{{$post->cover_image}}">
                     </div>
                     <div class="col-md-8 col-sm8">
-                        <h3><a href="posts/{{$post->id}}">{{$post->title}}</a></h3>
+                        <h3><a class="text-white text-decoration-none" href="posts/{{$post->id}}">{{$post->title}}</a></h3>
                     {{$post->body}}
                     <br>
                         <small>Writen on {{$post -> created_at}} by {{$post->user->name}}</small>
